2029601463c0dac9c04fa8a49f3df5d5270d12b1
Tri Ho <hominhtri0599@gmail.com>
2022-02-17 15:29:13 +0700

    set up go-admin

--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.16
 require github.com/jinzhu/gorm v1.9.16
 
 require (
-	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e // indirect
+	github.com/GoAdminGroup/go-admin v1.2.23
+	github.com/GoAdminGroup/themes v0.0.43
 	github.com/gin-gonic/gin v1.7.7
-	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/gobuffalo/packr/v2 v2.8.3 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/spf13/viper v1.10.1
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gorm.io/driver/postgres v1.2.3
 	gorm.io/gorm v1.22.5
 )
--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "github.com/smartschool/database"
+import (
+	_ "github.com/GoAdminGroup/go-admin/adapter/gin" // web framework adapter
+	"github.com/GoAdminGroup/go-admin/context"
+	"github.com/GoAdminGroup/go-admin/engine"
+	"github.com/GoAdminGroup/go-admin/modules/config"
+	"github.com/GoAdminGroup/go-admin/modules/db"
+	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/sqlite" // sql driver
+	"github.com/GoAdminGroup/go-admin/modules/language"
+	"github.com/GoAdminGroup/go-admin/template/types"
+	_ "github.com/GoAdminGroup/themes/adminlte" // ui theme
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	database.Init()
+	r := gin.Default()
+
+	// Instantiate a GoAdmin engine object.
+	eng := engine.Default()
+
+	// GoAdmin global configuration, can also be imported as a json file.
+	cfg := config.Config{
+		Databases: config.DatabaseList{
+			"default": {
+				File:       "./admin.db",
+				MaxIdleCon: 50,
+				MaxOpenCon: 150,
+				Driver:     db.DriverSqlite,
+			},
+		},
+		UrlPrefix: "admin",
+		IndexUrl:  "/",
+		Debug:     true,
+		Language:  language.EN,
+		Theme:     "adminlte",
+	}
+
+	// Add configuration and plugins, use the Use method to mount to the web framework.
+	_ = eng.AddConfig(&cfg).
+		Use(r)
+
+	eng.HTML("GET", "/admin", DashboardPage)
+
+	_ = r.Run(":9035")
+
+	// database.Init()
+}
+
+func DashboardPage(ctx *context.Context) (types.Panel, error) {
+	return types.Panel{
+		Content:     "hello world",
+		Title:       "Dashboard",
+		Description: "dashboard example",
+	}, nil
 }
