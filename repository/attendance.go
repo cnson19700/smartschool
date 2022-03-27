50ddc2b658543124067fd723bdc2643601ab81d1
Son Cao <son.cao@executionlab.asia>
2022-03-27 12:48:17 +0700

    create teacher + pagnitor

--- repository/attendance.go
+++ repository/attendance.go
@@ -1,17 +1,36 @@
 package repository
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/smartschool/database"
 	"github.com/smartschool/model/entity"
 )
 
-func QueryAttendanceByStudentSchedule(student_id uint, schedule_id uint) (*entity.Attendance, bool, error) {
+func QueryAttendanceByTeacherCourse(teacherID string, courseId string) ([]*entity.Attendance, error) {
+	attendance := []*entity.Attendance{}
+	err := database.DbInstance.Where("teacher_id = ? AND course_id =?", teacherID, courseId).Find(&attendance).Error
+	if err != nil {
+		return nil, err
+	}
+	return attendance, nil
+}
+
+func QueryAttendanceByCourseID(courseId string) ([]*entity.Attendance, error) {
+	attendance := []*entity.Attendance{}
+	err := database.DbInstance.Where("course_id =?", courseId).Find(&attendance).Error
+	if err != nil {
+		return nil, err
+	}
+	return attendance, nil
+}
+
+func QueryAttendanceByStudentSchedule(student_id string, schedule_id uint) (*entity.Attendance, error) {
 	var checkAttend entity.Attendance
 	result := database.DbInstance.Select("id").Where("user_id = ? AND schedule_id = ?", student_id, schedule_id).Find(&checkAttend)
 
-	return &checkAttend, result.RowsAffected == 0, result.Error
+	return &checkAttend, result.Error
 }
 
 func CreateAttendance(attendance entity.Attendance) error {
@@ -34,4 +53,20 @@ func QueryListAttendanceInDayByUser(user_id uint, start time.Time, end time.Time
 	result := database.DbInstance.Where("user_id = ? AND (checkin_time BETWEEN ? AND ?)", user_id, start, end).Preload("Schedule.Room").Preload("Schedule.Course").Find(&queryList)
 
 	return queryList, result.RowsAffected == 0, result.Error
+}
+
+func SearchAttendance(pagnitor *entity.Paginator, filter *entity.AttendanceFilter,
+	orders []string) ([]*entity.Attendance, error) {
+	query := database.DbInstance.Model(&entity.Attendance{})
+
+	//Order
+	for _, order := range orders {
+		query.Order(order)
+	}
+
+	fmt.Println(filter)
+
+	if filter.Keyword != "" { //search   checkin_time filter select?
+		query.Where("title LIKE ?", "%"+filter.Keyword+"%") //user_name, schedule_id, user_id, checkin_status
+	}
 }
