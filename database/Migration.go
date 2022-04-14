9b14970cd0e7e7093b21b83147c97c3b42b81e71
Son Cao <son.cao@executionlab.asia>
2022-04-14 15:44:19 +0700

    update QR Prefix

--- database/Migration.go
+++ database/Migration.go
@@ -30,8 +30,7 @@ func Close() {
 }
 
 func ConnectDatabase() {
-	//dbURI := "host=13.228.244.196 port=5432 user=busmapdb dbname=phenikaamaas_attendancedb sslmode=disable password=frjsdfhaflpzlcdzgnfvuxkdwiiiiklpojzowxajmendeeoqtbzyrgi"
-	dbURI := "host=localhost port=5432 user=postgres dbname=smartschool_local sslmode=disable password=12345678"
+	dbURI := "host=13.228.244.196 port=5432 user=busmapdb dbname=phenikaamaas_attendancedb sslmode=disable password=frjsdfhaflpzlcdzgnfvuxkdwiiiiklpojzowxajmendeeoqtbzyrgi"
 
 	var err error
 	DbInstance, err = gorm.Open(postgres.Open(dbURI), &gorm.Config{})
