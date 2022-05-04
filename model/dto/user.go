4e60f530a6f45b0e6f7680b3aea1886ba9520ee8
lhnhan <hoangnhan2506@gmail.com>
2022-05-04 17:56:09 +0700

    add change password first time

--- model/dto/user.go
+++ model/dto/user.go
@@ -4,3 +4,8 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type ChangePasswordFirstTimeRequest struct {
+	NewPass   string `json:"new_password"`
+	ReNewPass string `json:"re_new_password"`
+}
